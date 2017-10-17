5ac2a148e98161b310bff0d97166e3628a48d80b
Yasser Nascimento <yassern@protonmail.com>
2017-10-16 22:14:01 -0300

    add vivaldi browser

--- setup.sh
+++ setup.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+# Enable http command
+sudo apt-get install -y httpie
+
 # add repositories
 sudo add-apt-repository -y ppa:dawidd0811/neofetch
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo add-apt-repository -y ppa:webupd8team/atom
 sudo add-apt-repository -y ppa:saiarcot895/chromium-beta
-sudo add-apt-repository -y ppa:nilarimogard/webupd8
+sudo add-apt-repository -y ppa:nilarimogard/webupd8 # syspeek
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
+
+sudo sh -c 'echo "deb http://repo.vivaldi.com/stable/deb/ stable main" >> /etc/apt/sources.list'
+http --download http://repo.vivaldi.com/stable/linux_signing_key.pub | sudo apt-key add -
 
 # update apt
 sudo apt-get update
@@ -16,7 +22,7 @@ sudo apt-get upgrade
 sudo apt-get install -y ubuntu-restricted-extras libavcodec-extra rar unrar synaptic
 
 # shell
-sudo apt-get install -y git tmux httpie neofetch
+sudo apt-get install -y git tmux neofetch
 
 # programming languages
 http --download https://raw.githubusercontent.com/creationix/nvm/v0.33.5/install.sh | bash
@@ -32,7 +38,7 @@ apm install atom-beautify editorconfig emmet highlight-selected minimap pigments
   duotone-dark-forest-syntax duotone-dark-earth-syntax duotone-dark-sky-syntax
 
 # browsers
-sudo apt-get install -y chromium-browser
+sudo apt-get install -y chromium-browser vivaldi-stable
 
 # other apps
 sudo apt-get install -y syspeek bleachbit simplescreenrecorder
