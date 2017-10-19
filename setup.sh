af7f52efb4e231be36fd193a1d641e2f1358c177
Yasser Nascimento <yassern@protonmail.com>
2017-10-19 00:41:51 -0300

    uninstall some stuff

--- setup.sh
+++ setup.sh
@@ -48,3 +48,13 @@ sudo apt-get install -y chromium-browser vivaldi-stable
 
 # other apps
 sudo apt-get install -y syspeek bleachbit simplescreenrecorder
+
+# uninstall some stuff
+sudo apt-get purge -y transmission-common transmission-gtk deja-dup thunderbird simple-scan \
+  aisleriot gnome-sudoku gnome-mines gnome-mahjongg \
+  libreoffice-draw libreoffice-calc libreoffice-impress libreoffice-math \
+  gnome-orca onboard* vino remmina xterm xserver-xorg-input-all-hwe-16.04 \
+  unity-webapps-common webbrowser-app unity-lens-files unity-lens-music unity-lens-photos unity-lens-video \
+  unity-scope-chromiumbookmarks unity-scope-colourlovers unity-scope-devhelp unity-scope-firefoxbookmarks \
+  unity-scope-gdrive unity-scope-manpages unity-scope-openclipart unity-scope-texdoc \
+  unity-scope-tomboy unity-scope-video-remote unity-scope-virtualbox unity-scope-yelp unity-scope-zotero
